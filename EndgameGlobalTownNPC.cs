bfc1572ac43f23b8fe43b9f8eb49b6ab09e6690a
Zeerck <doroschkewich11@gmail.com>
2021-11-18 03:19:19 +0500

    Updated VS. Added Extra Chat Quotes for Guide.

---
+++ EndgameGlobalTownNPC.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.Localization;
+
+namespace Endgame
+{
+    public class EndgameGlobalTownNPC
+    {
+        public static void ExtraNPCQoutes(NPC npc, Mod mod, ref string chat)
+        {
+            int zeerckNPC = NPC.FindFirstNPC(ModContent.NPCType<NPCs.TownNPCs.NpcZeerck>());
+
+            switch(npc.type)
+            {
+                case 22:
+                    if (Utils.NextBool(Main.rand, 5) && EndgameWorld.ZeerckSpawn)
+                        chat = Language.GetTextValue("Mods.Endgame.ExtraGuideQuote1", Main.npc[zeerckNPC].GivenName);
+                    break;
+            }
+        }
+    }
+}
