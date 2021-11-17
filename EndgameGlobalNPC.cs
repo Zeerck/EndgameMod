9b3e9a40e62b31a53ddf94db42dc41533fe3d2dc
Zeerck <doroschkewich11@gmail.com>
2021-11-18 03:14:29 +0500

    Added Programer Soul, drops from any enemy for now.

---
+++ EndgameGlobalNPC.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace Endgame
+{
+    public class EndgameGlobalNPC : GlobalNPC
+    {
+        public override void GetChat(NPC npc, ref string chat) => EndgameGlobalTownNPC.ExtraNPCQoutes(npc, mod, ref chat);
+
+        public override void NPCLoot(NPC npc)
+        {
+            if (!npc.boss && !npc.friendly && npc.CanBeChasedBy())
+                EndgameDropper.DropItem(npc, ModContent.ItemType<Items.ProgramerSoul>(), 1, 10);
+            if(npc.boss)
+                EndgameDropper.DropItem(npc, ModContent.ItemType<Items.ProgramerSoul>(), 20000, 50000);
+        }
+    }
+}
---
+++ Items/ProgramerSoul.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace Endgame.Items
+{
+    public class ProgramerSoul : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault(Language.GetTextValue("Mods.Endgame.ProgramerSoulName"));
+            Tooltip.SetDefault(Language.GetTextValue("Mods.Endgame.ProgramerSoulDescription"));
+
+            Main.RegisterItemAnimation(item.type, new DrawAnimationVertical(5, 4));
+            ItemID.Sets.AnimatesAsSoul[item.type] = true;
+            ItemID.Sets.ItemIconPulse[item.type] = true;
+            ItemID.Sets.ItemNoGravity[item.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Item refItem = new Item();
+
+            refItem.SetDefaults(ItemID.SoulofSight);
+            item.width = refItem.width;
+            item.height = refItem.height;
+            item.maxStack = 999999;
+            item.value = 1000;
+            item.rare = ItemRarityID.Lime;
+        }
+
+        public override void GrabRange(Player player, ref int grabRange)
+        {
+            grabRange *= 3;
+        }
+
+        public override void PostUpdate()
+        {
+            Lighting.AddLight(item.position, Color.LightBlue.ToVector3() * 0.55f * Main.essScale);
+        }
+    }
+}
