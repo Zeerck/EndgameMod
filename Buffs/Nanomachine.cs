9f71baf1c512000297181a0fe81f12545af555c0
Zeerck <doroschkewich11@gmail.com>
2021-11-18 03:13:34 +0500

    Added Nanomachine mount (Didn't work texture idk why)

---
+++ Buffs/Nanomachine.cs
@@ -0,0 +1,23 @@
+﻿using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace Endgame.Buffs
+{
+    public class Nanomachine : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault(Language.GetTextValue("Mods.Endgame.NanomachineName"));
+            Description.SetDefault(Language.GetTextValue("Mods.Endgame.NanomachineMountDescription"));
+            Main.buffNoTimeDisplay[Type] = true;
+            Main.buffNoSave[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.mount.SetMount(ModContent.MountType<Mounts.Nanomachine>(), player);
+            player.buffTime[buffIndex] = 10;
+        }
+    }
+}
--- Items/Nanomachine.cs
+++ Items/Nanomachine.cs
@@ -16,11 +16,29 @@ namespace Endgame.Items
 
         public override void SetDefaults()
         {
-            item.width = 26;
-            item.height = 26;
-            
-            item.maxStack = 1;
-            item.rare = ItemRarityID.Expert;
+            item.width = 20;
+            item.height = 30;
+            item.useTime = 20;
+            item.useAnimation = 20;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.value = 30000;
+            item.rare = ItemRarityID.Lime;
+            item.UseSound = SoundID.Item79;
+            item.noMelee = true;
+            item.mountType = ModContent.MountType<Mounts.Nanomachine>();
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+
+            recipe.AddIngredient(ModContent.ItemType<ProgramerSoul>(), 57000);
+            recipe.AddIngredient(ItemID.Nanites, 450);
+            recipe.AddIngredient(ItemID.ChlorophyteBar, 5);
+            recipe.AddIngredient(ItemID.HallowedBar, 12);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
         }
     }
 }
